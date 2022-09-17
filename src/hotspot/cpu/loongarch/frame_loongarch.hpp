3e503002a909c8aa6ed1b2f35eccd7c5e1b8b6ea
loongson-jvm <jvm@loongson.cn>
2022-09-17 23:40:43 +0800

    Update to jdk-20+12
    27637: LA port of 8292267: Clean up synchronizer.hpp

--- src/hotspot/cpu/loongarch/frame_loongarch.hpp
+++ src/hotspot/cpu/loongarch/frame_loongarch.hpp
@@ -26,8 +26,6 @@
 #ifndef CPU_LOONGARCH_FRAME_LOONGARCH_HPP
 #define CPU_LOONGARCH_FRAME_LOONGARCH_HPP
 
-#include "runtime/synchronizer.hpp"
-
 // A frame represents a physical stack frame (an activation).  Frames can be
 // C or Java frames, and the Java frames can be interpreted or compiled.
 // In contrast, vframes represent source-level activations, so that one physical frame
